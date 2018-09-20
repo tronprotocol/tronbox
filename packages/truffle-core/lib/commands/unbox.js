7d8c05be6d8ccc5cf67ea7cb6378adbbffafa774
sullof <sullof@gmail.com>
2018-09-20 14:30:19 +0200

    This is a work-in-progress, not-working version. DO NOT MERGE

--- packages/truffle-core/lib/commands/unbox.js
+++ packages/truffle-core/lib/commands/unbox.js
@@ -5,7 +5,7 @@
  * - a string containing a repo under the `truffle-box` org
  */
 function normalizeURL(url) {
-  url = url || "https://github.com/simon4545/bare-box";
+  url = url || "https://github.com/sullof/bare-box";
 
   // full URL already
   if (url.indexOf("://") != -1 || url.indexOf("git@") != -1) {
@@ -20,7 +20,7 @@ function normalizeURL(url) {
     if (url.indexOf("-box") == -1) {
       url = url + "-box";
     }
-    return "https://github.com/simon4545/" + url;
+    return "https://github.com/sullof/" + url;
   }
 
   throw new Error("Box specified in invalid format");
