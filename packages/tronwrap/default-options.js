c96749885a232db8cd526cc7df08cb7f596883b5
sullof <sullof@gmail.com>
2018-10-11 14:03:14 -0700

    Skipping the solidity tests

--- packages/tronwrap/default-options.js
+++ packages/tronwrap/default-options.js
@@ -1,13 +1,10 @@
-
-// we use this for tests and for when TronWrap() is executed without passing the options
-
 module.exports = {
   from: 'TPL66VK2gCXNCD7EJg9pgJRfqcRazjhUZY',
   privateKey: 'da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0',
   consume_user_resource_percent: 30,
   fee_limit: 100000000,
-  fullNode: "https://api.trongrid.io:8090",
-  solidityNode: "https://api.trongrid.io:8091",
-  eventServer: "https://api.trongrid.io",
-  network_id: "*" // Match any network id
+  fullNode: "https://api.shasta.trongrid.io",
+  solidityNode: "https://api.shasta.trongrid.io",
+  eventServer: "https://api.shasta.trongrid.io",
+  network_id: "*"
 }
