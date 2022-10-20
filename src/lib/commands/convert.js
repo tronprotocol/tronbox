cfb5d1918a6c81b5538f2ab467d3006b44037058
startan <star.tan@tron.network>
2022-10-20 11:29:57 +0800

    init command support replacing web3 to tronWeb

--- src/lib/commands/convert.js
+++ src/lib/commands/convert.js
@@ -31,12 +31,48 @@ const command = {
       });
     }
 
+    function convertTests() {
+      const testDirPath = path.join(process.cwd(), './test');
+      if (!fs.existsSync(testDirPath)) return;
+      const stats = fs.statSync(testDirPath);
+      if (!stats.isDirectory()) return;
+
+      const gogocode = require('gogocode');
+      console.log(gogocode)
+      function translateTestFile(file) {
+        const content = fs.readFileSync(file).toString();
+        const translatedContent = gogocode(content)
+          .replace('web3', 'tronWeb')
+          .replace('tronWeb.eth', 'tronWeb.trx')
+          .root()
+          .generate();
+        if (content === translatedContent) return;
+        fs.writeFileSync(file, translatedContent);
+        config.logger.log(`convert file ${file} successful`);
+      }
+
+      function walkTestDir(dir) {
+        const files = fs.readdirSync(dir);
+        files.forEach((file) => {
+          const currentFile = path.join(dir, file);
+          const stats = fs.statSync(currentFile);
+          if (stats.isDirectory()) return walkTestDir(currentFile);
+          if (!stats.isFile()) return;
+          if (path.extname(currentFile) !== '.js') return;
+          translateTestFile(currentFile);
+        });
+      }
+
+      walkTestDir(testDirPath);
+    }
+
     downloadConfig()
       .then(() => {
         config.logger.log('OK. Convertion is done. Enjoy tronbox!');
         config.logger.log();
         done();
       });
+    convertTests();
     
   },
 };
