11cf7782c721a6c666f49e35441f753037c974c2
startan <star.tan@tron.network>
2022-11-03 17:30:20 +0800

    add test convert

--- test/convert/test/testConvert.js
+++ test/convert/test/testConvert.js
@@ -11,6 +11,20 @@ async function wait(time) {
 }
 
 contract('MetaCoinConvert', accounts => {
+
+  function jsonrpcRequest(method, params = [], X = 'post') {
+    return tronWeb.fullNode.request(
+      '/jsonrpc',
+      {
+        jsonrpc: '2.0',
+        id: 1,
+        method,
+        params,
+      },
+      X
+    ).then((resp) => resp.result);
+  }
+
   describe('test utils', () => {
     it('function that tronWeb has: utils.sha3 ', async () => {
       let sha3_ = await web3.utils.sha3('234');
@@ -48,27 +62,41 @@ contract('MetaCoinConvert', accounts => {
   });
 
   describe('test eth', () => {
+    
     it('should get the right balance', async () => {
-      let balance2 = await web3.eth.getBalance(accounts[2]);
-      assert(typeof balance2 === 'number', 'eth.getBalance should return a number');
+      const balance1 = await web3.eth.getBalance(accounts[2]);
+      const balance2 = await jsonrpcRequest('eth_getBalance', [accounts[2]]);
+      assert.equal(balance1, balance2, 'eth.getBalance should get the balance of accounts[2]');
     });
 
     it('should get the receipt', async () => {
       const metaCoinInstance = await MetaCoin.deployed();
       const sendCoinTxId = await metaCoinInstance.sendCoin(accounts[1], 10, { from: accounts[0] });
       await wait(3000); // wait for broadcasting
-      const receipt = await web3.eth.getTransactionReceipt(sendCoinTxId);
-      assert.equal(sendCoinTxId, receipt.transactionHash.replace(/^0x/, ''), 'transaction receipt is not match');
+      const receipt1 = await web3.eth.getTransactionReceipt(sendCoinTxId);
+      const receipt2 = await jsonrpcRequest('eth_getTransactionReceipt', [sendCoinTxId]);
+      assert.equal(sendCoinTxId, receipt1.transactionHash.replace(/^0x/, ''), 'transaction receipt is not match');
+      assert.equal(
+        receipt1.transactionHash.replace(/^0x/, ''),
+        receipt2.transactionHash.replace(/^0x/, ''),
+        'transaction receipt is not match'
+      );
     });
 
     it('should get the first block', async () => {
-      const block = await web3.eth.getBlock(1);
-      assert.equal(1, block.block_header.raw_data.number, "did't get the right block");
+      const block1 = await web3.eth.getBlock(1);
+      const block2 = await jsonrpcRequest('eth_getBlockByNumber', [1]);
+      assert.equal(
+        block1.block_header.raw_data.number,
+        block2.block_header.raw_data.number,
+        "did't get the right block"
+      );
     });
 
     it('should get the protocol version', async () => {
-      const protocolVersion = await web3.eth.getProtocolVersion();
-      assert.equal(0x18, protocolVersion, 'protocol version not match');
+      const protocolVersion1 = await web3.eth.getProtocolVersion();
+      const protocolVersion2 = await jsonrpcRequest('eth_protocolVersion')
+      assert.equal(protocolVersion1, protocolVersion2, 'protocol version not match');
     });
 
     it('should get accounts', async () => {
@@ -77,22 +105,27 @@ contract('MetaCoinConvert', accounts => {
     });
 
     it('should get syncing status', async () => {
-      const syncingStatus = await web3.eth.isSyncing();
-      assert(syncingStatus !== undefined, 'did not get the syncing status');
+      const syncingStatus1 = await web3.eth.isSyncing();
+      const syncingStatus2 = await jsonrpcRequest('eth_syncing');
+      assert.deepEqual(syncingStatus1, syncingStatus2, 'did not get the syncing status');
     });
 
     it('should get storage at some where', async () => {
-      const storedData = await web3.eth.getStorageAt(accounts[2]);
-      assert.equal(storedData, 1, 'did not get the right stored data');
+      const storedData1 = await web3.eth.getStorageAt(accounts[2]);
+      const storedData2 = await jsonrpcRequest('eth_getStorageAt', [accounts[2]]);
+      assert.deepEqual(storedData1, storedData2, 'did not get the right stored data');
     });
   });
 
   describe('test tre', () => {
     it('set account balance', async () => {
-      const originBalance = await web3.eth.getBalance(accounts[2]);
+      const originBalance = await jsonrpcRequest('eth_getBalance', [accounts[2]]);
+      console.log(originBalance);
       await provider.send('evm_setAccountBalance', [accounts[2], originBalance + 1]);
       await wait(6000);
-      assert.equal(originBalance + 1, await web3.eth.getBalance(accounts[2]), 'set balance failed');
+      assert.equal(originBalance + 1, jsonrpcRequest('eth_getBalance', [accounts[2]]), 'set balance failed');
     });
   });
 });
+
+        
