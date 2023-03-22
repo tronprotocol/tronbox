7fc70c4b7c9fdfbc32c6156bd66e0889364b3008
llwslc <llwslc@gmail.com>
2023-03-22 17:20:56 +0800

    test: add note

--- test/openzeppelin/beacon/contracts/BeaconBoxV2.sol
+++ test/openzeppelin/beacon/contracts/BeaconBoxV2.sol
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: MIT
+// NOTE: Do not use this code snippet, it's incomplete and has a critical vulnerability! Only for test cases.
+
 pragma solidity >=0.4.22 <0.9.0;
 
 contract BeaconBoxV2 {
